e0cea1adbbb4253cd0124e34be34cc078a2e9a1f
Robert MacLeod <sarca571ca@gmail.com>
2026-02-20 23:07:56 -0500

    Added avatar support on load for Enki

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,13 @@
 import { Events } from "discord.js"
 import { ClientWithCommands } from "../types/ClientWithCommands";
+import * as fs from "node:fs/promises";
 
 export const name = Events.ClientReady;
 export const once = true;
 
-export const execute = (client: ClientWithCommands) => {
+export const execute = async (client: ClientWithCommands) => {
+    const avatar = await fs.readFile("../assets/enki.png");
+    await client.user?.setAvatar(avatar);
     console.log(`Ready! Logged in as ${client.user?.username}.`)
 };
 
